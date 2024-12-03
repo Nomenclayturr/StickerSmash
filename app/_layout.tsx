e65f24c0967f2d9c011aaaa7d0aa34b0e5004c32
Emmanuel <limpbuiscuit@gmail.com>
2024-12-02 20:51:23 -0600

    app.tsx and components

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,19 @@
-import { Stack } from 'expo-router';
+import { Stack } from "expo-router";
+
+import { StatusBar } from "expo-status-bar";
+import * as SplashScreen from 'expo-splash-screen';
+
 
 export default function RootLayout() {
+  SplashScreen.preventAutoHideAsync();
+setTimeout(SplashScreen.hideAsync, 5000);
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-    </Stack>
+    <>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="light" />
+    </>
   );
 }
